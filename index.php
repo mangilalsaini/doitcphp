9bd1077e891cc607b7eedb0fb6edccd25b2dfcab
mangilalsaini <113425979+mangilalsaini@users.noreply.github.com>
2022-09-26 21:45:41 +0530

    total, active, inactive user's count print

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include_once("includes/header.php") ?>
+<?php 
+    include_once("includes/header.php");
+    include_once("includes/dbconnection.php");
+?>
 
     <!-- Page Wrapper -->
     <div id="wrapper">
@@ -37,11 +40,19 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                                Total User's</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php
+                                                    $query = "SELECT * FROM `users` WHERE 1;";
+                                                    $result = mysqli_query($conn, $query);
+                                                    $user_all = mysqli_num_rows($result);
+                                                    echo $user_all;
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                            <img src="images/user_all.png" width="40px" height="40px"/>
+                                            <!--   <i class="fas fa-users" aria-hidden="true"></i>   -->
                                         </div>
                                     </div>
                                 </div>
@@ -55,11 +66,31 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Active User's</div>
+                                                <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                        <?php
+                                                            $query = "SELECT * FROM `users` WHERE `active`=1;";
+                                                            $result = mysqli_query($conn, $query);
+                                                            $user_active = mysqli_num_rows($result);
+                                                            echo $user_active;
+                                                            $area_value = round(($user_active * 100)/$user_all,0);
+                                                        ?>   
+                                                    </div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="progress progress-sm mr-2">
+                                                        <div class="progress-bar bg-info" role="progressbar"
+                                                            style="width: <?= $area_value ?>%" aria-valuenow="<?= $area_value ?>" aria-valuemin="0"
+                                                            aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                            <img src="images/user_active.png" width="40px" height="40px"/>
+                                            <!--   <i class="fas fa-users" aria-hidden="true"></i>   -->
                                         </div>
                                     </div>
                                 </div>
@@ -72,23 +103,32 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><label style="color:red"> Inactive User's</label>
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                        <?php
+                                                            $query = "SELECT * FROM `users` WHERE `active`=0;";
+                                                            $result = mysqli_query($conn, $query);
+                                                            $user_inactive = mysqli_num_rows($result);
+                                                            echo $user_inactive;
+                                                            $area_value = round(($user_inactive * 100)/$user_all,0);
+                                                        ?>   
+                                                    </div>
                                                 </div>
                                                 <div class="col">
                                                     <div class="progress progress-sm mr-2">
                                                         <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                                            style="width: <?= $area_value ?>%" aria-valuenow="<?= $area_value ?>" aria-valuemin="0"
                                                             aria-valuemax="100"></div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                            <img src="images/user_inactive.png" width="40px" height="40px"/>
+                                            <!--   <i class="fas fa-users" aria-hidden="true"></i>   -->
                                         </div>
                                     </div>
                                 </div>
